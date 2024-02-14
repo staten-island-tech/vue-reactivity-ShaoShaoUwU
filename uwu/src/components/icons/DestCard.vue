2bdd769f605533a6147825b8c7ba4adb4ad3ca81
Shao <leo21604212@gmail.com>
2024-02-14 13:11:58 -0500

    iubgjkm

--- uwu/src/components/icons/DestCard.vue
+++ uwu/src/components/icons/DestCard.vue
@@ -5,12 +5,33 @@
 <button @click="increment"> Clicker</button>
 <h3>{{ clicked }}</h3>
     </div>
+    <template>
+    <div>
+        <h2>{{ Whale.name }}</h2>
+        <img :src="Whale.img" alt="">
+        <button @click="increment">Add Whale</button>
+        <h3>{{ clicked }}</h3>
+    </div>
 </template>
 
-<script>
-    
+<script setup>
+import {ref} from "vue";
+const props = defineProps({
+    Destination: Object,
+});
+
+//clicker logic
+const clicked = ref(0);
+function increment(){
+    clicked.value++;
+}
 </script>
 
 <style scoped>
+img {
+    width: 300px;
+    height: 300px;
+
+}
 
 </style>
