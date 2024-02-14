3d1618fadc608034a5c862c9b165f0593235ad65
Shao <leo21604212@gmail.com>
2024-02-14 12:44:12 -0500

    www

---
+++ uwu/src/components/icons/DestCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+<h2>{{ Destination.name }}</h2>
+<img :src="Destination.img" alt="">
+<button @click="increment"> Clicker</button>
+<h3>{{ clicked }}</h3>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+
+</style>
