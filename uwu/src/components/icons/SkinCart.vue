b17f213fe4e210d4185a21b50de571eea7bd352e
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-03-18 08:04:58 -0400

    ea

---
+++ uwu/src/components/icons/SkinCart.vue
@@ -0,0 +1,15 @@
+<div class="cart">
+    <h2>{{ destination.skins }} - {{ destination.price }} x {{ destination.clicked }}</h2>
+</div>
+</template>
+
+<script setup>
+const props = defineProps({
+destination: Object,
+});
+
+</script>
+
+<style scoped>
+
+</style>
